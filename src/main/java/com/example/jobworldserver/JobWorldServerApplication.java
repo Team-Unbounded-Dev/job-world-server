2204924c3d6acc052b7fbcf3383b62c2ea46543c
valhub08 <jhstl1203@gmail.com>
2025-05-12 09:09:46 +0900

    feat :: first commit

---
+++ src/main/java/com/example/jobworldserver/JobWorldServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.jobworldserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobWorldServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobWorldServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jobworldserver/JobWorldServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jobworldserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobWorldServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

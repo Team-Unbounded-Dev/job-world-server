54d7a5408f5110f468482986bf80ff5cf40947bf
valhub08 <jhstl1203@gmail.com>
2025-05-14 23:22:52 +0900

    feat :: Job.java(Job 클래스 추가 )

---
+++ src/main/java/com/example/jobworldserver/domain/auth/entity/Job.java
@@ -0,0 +1,19 @@
+package com.example.jobworldserver.domain.auth.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Job {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+}

7869de65860acee8ca15b02d47fe6ad589794964
valhub08 <jhstl1203@gmail.com>
2025-05-14 20:57:00 +0900

    feat :: User.java(Auth 관련 entity 코드)

---
+++ src/main/java/com/example/jobworldserver/domain/auth/entity/User.java
@@ -0,0 +1,67 @@
+package com.example.jobworldserver.domain.auth.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Authority authority;
+
+    @Column(nullable = true)
+    private String school;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "job_id")
+    private Job job;
+
+    @Column(nullable = true)
+    private Integer age;
+
+    @Column(nullable = true)
+    private Long grade;
+
+    @Column(nullable = true)
+    private Long classNum;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "teacher_id")
+    private User teacher;
+
+    @Column(unique = true, nullable = true)
+    private String email;
+
+    @Builder.Default
+    private Boolean emailVerified = false;
+
+    public void verifyEmail() {
+        this.emailVerified = true;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setJob(Job job) {
+        this.job = job;
+    }
+}

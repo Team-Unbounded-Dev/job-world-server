7b42f54a63c07509c2f523699ee9751248334288
valhub08 <jhstl1203@gmail.com>
2025-05-14 23:02:57 +0900

    feat :: UserRepository.java(User Nickname 정보 가져오기)

---
+++ src/main/java/com/example/jobworldserver/domain/auth/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.jobworldserver.domain.auth.repository;
+
+import com.example.jobworldserver.domain.auth.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByNickname(String nickname);
+}

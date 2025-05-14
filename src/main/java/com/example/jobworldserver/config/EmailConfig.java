7a5c276824907760281437e767c8809947b094c0
valhub08 <jhstl1203@gmail.com>
2025-05-14 23:29:20 +0900

    feat :: EmailConfig.java(EmailConfig 클래스 추가)

---
+++ src/main/java/com/example/jobworldserver/config/EmailConfig.java
@@ -0,0 +1,55 @@
+package com.example.jobworldserver.config;
+
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Slf4j
+@Configuration
+public class EmailConfig {
+
+    @Value("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+    @Value("${spring.mail.default-encoding:UTF-8}")
+    private String defaultEncoding;
+
+    @Value("${spring.mail.properties.mail.smtp.auth:true}")
+    private String auth;
+
+    @Value("${spring.mail.properties.mail.smtp.starttls.enable:true}")
+    private String starttls;
+
+    @PostConstruct
+    public void init() {
+        log.info("EmailConfig - host: {}, username: {}, defaultEncoding: {}", host, username, defaultEncoding);
+    }
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(host);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+        mailSender.setDefaultEncoding(defaultEncoding);
+
+        Properties properties = new Properties();
+        properties.setProperty("mail.smtp.auth", auth);
+        properties.setProperty("mail.smtp.starttls.enable", starttls);
+        mailSender.setJavaMailProperties(properties);
+
+        return mailSender;
+    }
+}

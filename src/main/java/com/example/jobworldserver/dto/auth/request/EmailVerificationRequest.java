3ac044d07b134477b842530b285c26961dbd54ca
valhub08 <jhstl1203@gmail.com>
2025-05-28 20:14:42 +0900

    refactor :: EmailVerificationRequest(임시 id 필드 추가)

--- src/main/java/com/example/jobworldserver/dto/auth/request/EmailVerificationRequest.java
+++ src/main/java/com/example/jobworldserver/dto/auth/request/EmailVerificationRequest.java
@@ -9,4 +9,7 @@ public class EmailVerificationRequest {
     @NotBlank(message = "이메일은 필수입니다.")
     @Email(message = "유효한 이메일 형식이어야 합니다.")
     private String email;
+
+    @NotBlank
+    private String tempUserId;
 }

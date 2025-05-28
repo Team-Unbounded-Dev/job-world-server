c9a8f83123e2f8117bb16214bc37c94c03f94213
valhub08 <jhstl1203@gmail.com>
2025-05-28 20:40:30 +0900

    refactor :: OAuth2FailureHandler(json형식으로 변경)

--- src/main/java/com/example/jobworldserver/oauth/handler/OAuth2FailureHandler.java
+++ src/main/java/com/example/jobworldserver/oauth/handler/OAuth2FailureHandler.java
@@ -1,9 +1,11 @@
-package com.example.jobworldserver.domain.auth.oauth;
+package com.example.jobworldserver.oauth.handler;
 
 import com.example.jobworldserver.dto.auth.common.ApiResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
@@ -14,7 +16,10 @@ import java.io.IOException;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class OAuth2FailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+    private final ObjectMapper objectMapper;
 
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
@@ -22,6 +27,11 @@ public class OAuth2FailureHandler extends SimpleUrlAuthenticationFailureHandler 
         log.error("OAuth2 로그인 실패: {}", exception.getMessage());
         log.error("요청 URL: {}", request.getRequestURL());
         log.error("파라미터: {}", request.getQueryString());
-        ApiResponse.failure("OAuth2 인증 실패: " + exception.getMessage(), HttpStatus.UNAUTHORIZED);
+
+        response.setContentType("application/json; charset=UTF-8");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.getWriter().write(objectMapper.writeValueAsString(
+                ApiResponse.failure("OAuth2 인증 실패: " + exception.getMessage(), HttpStatus.UNAUTHORIZED)
+        ));
     }
 }

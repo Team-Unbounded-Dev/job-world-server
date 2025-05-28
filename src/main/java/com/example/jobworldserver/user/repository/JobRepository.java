278c3c0df7b202f0a001151985d38ba26df36250
valhub08 <jhstl1203@gmail.com>
2025-05-28 20:21:02 +0900

    refactor :: JobRepository(id 조회 메서드 추가)

--- src/main/java/com/example/jobworldserver/user/repository/JobRepository.java
+++ src/main/java/com/example/jobworldserver/user/repository/JobRepository.java
@@ -1,10 +1,11 @@
-package com.example.jobworldserver.domain.auth.repository;
+package com.example.jobworldserver.user.repository;
 
-import com.example.jobworldserver.domain.auth.entity.Job;
+import com.example.jobworldserver.auth.entity.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface JobRepository extends JpaRepository<Job, Long> {
     Optional<Job> findByName(String name);
+    Optional<Job> findById(Long id);
 }

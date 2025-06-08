0008826a0e5c6032f1d8b5bd62ea206376826082
valhub08 <jhstl1203@gmail.com>
2025-06-08 17:47:23 +0900

    Style(💄) :: UserService.java

--- src/main/java/com/example/jobworldserver/user/service/UserService.java
+++ src/main/java/com/example/jobworldserver/user/service/UserService.java
@@ -6,10 +6,16 @@ import java.util.Optional;
 
 public interface UserService {
     User findByNickname(String nickname);
+
     Optional<User> findByEmail(String email);
+
     Optional<User> findById(Long id);
+
     Long registerTempUser(RegisterRequest request); // 임시 사용자 등록
+
     void updateTempUserEmail(Long tempUserId, String email); // 이메일 업데이트
+
     User registerOAuth2User(String email, String name, String provider);
+
     void verifyEmail(String email);
 }

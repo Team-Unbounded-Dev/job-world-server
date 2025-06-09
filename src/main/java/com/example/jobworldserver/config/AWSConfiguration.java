e6948ec04c62f15c10bc03b1751fd7f089003b36
valhub08 <jhstl1203@gmail.com>
2025-06-09 20:05:11 +0900

    feat :: iam 자격 증명 코드 추가

---
+++ src/main/java/com/example/jobworldserver/config/AWSConfiguration.java
@@ -0,0 +1,21 @@
+package com.example.jobworldserver.config;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AWSConfiguration {
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Bean
+    public AmazonS3 amazonS3() {
+        return AmazonS3ClientBuilder.standard()
+                .withRegion(region)
+                .build();
+    }
+}

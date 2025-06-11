71b6fd765fa216b5a67c23d8fb4d536a9c79708f
valhub08 <jhstl1203@gmail.com>
2025-06-11 20:30:26 +0900

    refactor :: S3 client AWS SDK v2 마이그레이션

--- src/main/java/com/example/jobworldserver/config/AWSConfiguration.java
+++ src/main/java/com/example/jobworldserver/config/AWSConfiguration.java
@@ -1,21 +1,23 @@
 package com.example.jobworldserver.config;
 
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class AWSConfiguration {
 
-    @Value("${cloud.aws.region.static}")
+    @Value("${cloud.aws.region.static:ap-northeast-2}")
     private String region;
 
     @Bean
-    public AmazonS3 amazonS3() {
-        return AmazonS3ClientBuilder.standard()
-                .withRegion(region)
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
 }

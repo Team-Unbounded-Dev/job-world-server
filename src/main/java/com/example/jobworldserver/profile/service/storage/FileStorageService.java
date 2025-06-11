3758168bb706fe2ab734a411163505065db7e942
valhub08 <jhstl1203@gmail.com>
2025-06-11 20:33:19 +0900

    refactor :: MultipartFile 사용 및 IOException 예외 처리 추가

--- src/main/java/com/example/jobworldserver/profile/service/storage/FileStorageService.java
+++ src/main/java/com/example/jobworldserver/profile/service/storage/FileStorageService.java
@@ -1,5 +1,9 @@
 package com.example.jobworldserver.profile.service.storage;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
 public interface FileStorageService {
-    String storeFile(String fileName, byte[] fileData);
+    String storeFile(String fileName, MultipartFile file) throws IOException;
 }

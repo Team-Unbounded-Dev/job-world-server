a89a63b03d6bdc87926a13cf75873f32e1f52a39
valhub08 <jhstl1203@gmail.com>
2025-06-11 20:41:23 +0900

    feat/remove(profile) :: MultipartFile 및 SDK V2로 변경

--- src/main/java/com/example/jobworldserver/profile/service/storage/S3FileStorageService.java
+++ src/main/java/com/example/jobworldserver/profile/service/storage/S3FileStorageService.java
@@ -1,50 +1,59 @@
 package com.example.jobworldserver.profile.service.storage;
 
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.S3Exception;
 
-import java.io.ByteArrayInputStream;
+import java.io.IOException;
 
 @Service
 public class S3FileStorageService implements FileStorageService {
 
     private static final Logger logger = LoggerFactory.getLogger(S3FileStorageService.class);
 
-    private final AmazonS3 amazonS3;
+    private final S3Client s3Client;
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucketName;
 
-    public S3FileStorageService(AmazonS3 amazonS3) {
-        this.amazonS3 = amazonS3;
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    public S3FileStorageService(S3Client s3Client) {
+        this.s3Client = s3Client;
     }
 
     @Override
-    public String storeFile(String fileName, byte[] fileData) {
+    public String storeFile(String fileName, MultipartFile file) throws IOException {
         try {
             String key = "profiles/" + fileName;
             logger.info("S3에 파일 업로드 시작: 버킷={}, 키={}", bucketName, key);
 
-            ObjectMetadata metadata = new ObjectMetadata();
-            metadata.setContentLength(fileData.length);
-            metadata.setContentType("image/jpeg");
+            PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(key)
+                    .contentType(file.getContentType())
+                    .contentLength(file.getSize())
+                    .build();
 
-            PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, key, new ByteArrayInputStream(fileData), metadata)
-                    .withCannedAcl(CannedAccessControlList.PublicRead);
+            s3Client.putObject(putObjectRequest, RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
 
-            amazonS3.putObject(putObjectRequest);
-            String fileUrl = amazonS3.getUrl(bucketName, key).toString();
-            logger.info("S3 파일 업로드 성공: URL={}", fileUrl);
+            String publicUrl = String.format("https://%s.s3.%s.amazonaws.com/%s", bucketName, region, key);
 
-            return fileUrl;
+            logger.info("S3 파일 업로드 성공: 퍼블릭 URL={}", publicUrl);
+
+            return publicUrl;
+        } catch (S3Exception e) {
+            logger.error("S3 파일 업로드 실패: 파일명={}, S3 오류={}", fileName, e.awsErrorDetails().errorMessage(), e);
+            throw new RuntimeException("파일 저장 실패: " + e.awsErrorDetails().errorMessage());
         } catch (Exception e) {
-            logger.error("S3 파일 업로드 실패: 파일명={}", fileName, e);
+            logger.error("S3 파일 업로드 실패: 파일명={}, 오류={}", fileName, e.getMessage(), e);
             throw new RuntimeException("파일 저장 실패: " + e.getMessage());
         }
     }
